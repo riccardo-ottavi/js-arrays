691cb3a1bdabcaef8d047b7a980355a988dd3366
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-29 14:24:23 +0200

    refactoring punto 3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -28,7 +28,14 @@ for(let i = 0; i < teachers.length; i++){
 }
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+for (let i = 0 ; i < teachers.length; i++) {
+  if (i === teachers.length-1) {
+    const lastTeacher = teachers[i];
+    teachers.length = teachers.length-1;
+    console.log(lastTeacher);
+  }
+}
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
