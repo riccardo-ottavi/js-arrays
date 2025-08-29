7d0674d6bee19da321d1c1a35f14be8d7896b8e8
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-29 14:00:50 +0200

    refactoring punto 1

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,22 +10,22 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[3];
-console.log(fourthTeacher);
+
+for(let i = 0; i < teachers.length; i++){
+  if (teachers[i] === teachers[3]) {
+    const fourthTeacher = teachers[3];
+    console.log(fourthTeacher);
+  }
+}
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-fifthTeacher = teachers[4] = "Patrick";
-console.log(teachers);
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers.pop(length-1);
-console.log(teachers);
-console.log(lastTeacher);
+const lastTeacher = null;
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers.shift();
-console.log(teachers);
-console.log(firstTeacher);
+const firstTeacher = null;
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
