46d96736f2be048fe2fba833de6864a3a93a70db
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-29 14:02:07 +0200

    refactoring punto 2

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -20,6 +20,12 @@ for(let i = 0; i < teachers.length; i++){
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+for(let i = 0; i < teachers.length; i++){
+  if (teachers[i] === teachers[4]) {
+    teachers[4] = "Patrick";
+    console.log(teachers);
+  }
+}
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
