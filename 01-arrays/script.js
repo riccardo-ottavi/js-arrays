458a0cce8889e2373ed4b70ca314f95d205a9e9f
RiccardoOttavi <ottavi.ps@libero.it>
2025-08-29 15:15:26 +0200

    risolvi esercizio 5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,7 +14,7 @@ const teachers = [
 for(let i = 0; i < teachers.length; i++){
   if (teachers[i] === teachers[3]) {
     const fourthTeacher = teachers[3];
-    console.log(fourthTeacher);
+    console.log("esercizio 1 " + fourthTeacher);
   }
 }
 
@@ -23,7 +23,7 @@ for(let i = 0; i < teachers.length; i++){
 for(let i = 0; i < teachers.length; i++){
   if (teachers[i] === teachers[4]) {
     teachers[4] = "Patrick";
-    console.log(teachers);
+    console.log("esercizio 2 " + teachers);
   }
 }
 
@@ -32,22 +32,24 @@ for (let i = 0 ; i < teachers.length; i++) {
   if (i === teachers.length-1) {
     const lastTeacher = teachers[i];
     teachers.length = teachers.length-1;
-    console.log(lastTeacher);
+    console.log("esercizio 3 " +lastTeacher);
   }
 }
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers[0];
-console.log(firstTeacher);
+console.log("esercizio 4 " + firstTeacher);
 for (let i = 0 ; i< teachers.length ; i++) {
   teachers[i] = teachers[i+1];
   if (i === teachers.length-1) {
     teachers.length = teachers.length-1;
   }
 }
-console.log(teachers);
+console.log("esercizio 4 " + teachers);
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers[teachers.length] = "Vanessa";
+console.log("Esercizio 5 " + teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
